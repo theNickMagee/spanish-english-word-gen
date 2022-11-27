20a7c326d37caecc417688b7fabac9e385d14164
nmagee_wsbeng <nmagee@wsbeng.com>
2022-11-27 16:03:20 -0600

    functionality done but look not there

--- eng-span-words/src/App.vue
+++ eng-span-words/src/App.vue
@@ -1,9 +1,33 @@
-<script setup>
+<script >
+import { store } from './store.js';
+import translate from "translate";
+import randomWords from 'random-words';
 
+// translate.engine = "deepl";
+// translate.key = process.env.DEEPL_KEY;
+
+
+
+
+export default {
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    async generateNewWord() {
+      this.store.englishWord = randomWords();
+      this.store.spanishWord = await translate(this.store.englishWord, "es");
+    }
+  }
+}
 </script>
 
 <template>
-  Hello world
+  <div>{{ this.store.englishWord }}</div>
+  <div>{{ this.store.spanishWord }}</div>
+  <div @click="generateNewWord">Cick me</div>
 </template>
 
 <style scoped>
---
+++ eng-span-words/src/store.js
@@ -0,0 +1,7 @@
+import { reactive } from 'vue';
+
+
+export const store = reactive({
+    englishWord: "Hello World",
+    spanishWord: "Hola Mundo"
+})
